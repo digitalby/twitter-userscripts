247a8b8e8bfb696fa194517dda3e9b11decf0d22
digitalby <yury@digitalby.me>
2026-02-09 23:05:28 +0100

    Fix backspace hotkey going back when no back button is present

--- twitter-backspace-back.user.js
+++ twitter-backspace-back.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter - Backspace to Go Back
 // @namespace    https://github.com/digitalby
-// @version      1.0.1
+// @version      1.0.2
 // @author       digitalby
 // @description  Press Backspace to navigate back on Twitter/X
 // @match        https://twitter.com/*
@@ -19,13 +19,11 @@
         const isEditable = document.activeElement.isContentEditable;
         if (tag === 'INPUT' || tag === 'TEXTAREA' || isEditable) return;
 
-        e.preventDefault();
-        // Use Twitter's soft navigation back button if available
+        // Only act if Twitter's back button is visible
         const backBtn = document.querySelector('[data-testid="app-bar-back"]');
-        if (backBtn) {
-            backBtn.click();
-        } else {
-            history.back();
-        }
+        if (!backBtn || backBtn.offsetParent === null) return;
+
+        e.preventDefault();
+        backBtn.click();
     });
 })();
